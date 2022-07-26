f6d5d9f1365281861efb7a01fb64430a9be23071
Kevin Brey <manbearwiz@gmail.com>
2022-07-26 14:08:25 -0500

    docs(*): add docs to Vexillology class

--- src/vexillology.ts
+++ src/vexillology.ts
@@ -3,14 +3,29 @@ import { VexillologyClient } from './models';
 export class Vexillology {
   constructor(private client: VexillologyClient) {}
 
+  /**
+   * Returns a Promise that tracks the client's initialization process.
+   * @returns A Promise that will resolve once the client is done initializing.
+   */
   async ready(): Promise<unknown> {
     return this.client.ready();
   }
 
+  /**
+   * Returns the value of the feature flag for the current user.
+   * @param key - The unique key of the feature flag to get the value from.
+   * @returns The value of the feature flag.
+   */
   get(key: string): unknown {
     return this.client.get(key);
   }
 
+  /**
+   * Tracks an event for use in an A/B test.
+   * @param eventKey The key of the event to be tracked.
+   * @param metricValue An optional numeric value to associate with the event.
+   * @param attributes A map of custom key-value pairs specifying additional information to associate with the event.
+   */
   track(
     eventKey: string,
     metricValue?: number | undefined,
