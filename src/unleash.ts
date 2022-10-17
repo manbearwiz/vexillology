cafb934262afc20df2d891cc031077d9ebad9569
Kevin Brey <kevin.brey@kbx.global>
2022-10-17 14:13:04 -0500

    feat(unleah): add unleash support

---
+++ src/unleash.ts
@@ -0,0 +1,47 @@
+import {
+  IConfig,
+  UnleashClient as UnleashProxyClient,
+} from 'unleash-proxy-client';
+
+import { UserAttributes, VexillologyClient } from './models';
+
+export class UnleashClient implements VexillologyClient {
+  private readonly client: UnleashProxyClient;
+  private readonly starting: Promise<void>;
+
+  constructor(config: IConfig) {
+    this.client = new UnleashProxyClient(config);
+    this.starting = this.client.start();
+  }
+
+  async changeUser(id: string, attributes: UserAttributes): Promise<void> {
+    return this.client.updateContext({
+      userId: id,
+      properties: Object.fromEntries(
+        Object.entries(attributes).map(([k, v]) => [k, v.toString()]),
+      ),
+    });
+  }
+
+  async ready(): Promise<unknown> {
+    return this.starting;
+  }
+
+  get(key: string): unknown {
+    return this.client.getVariant(key);
+  }
+
+  onUpdate(listener: () => void) {
+    this.client.on('update', listener);
+  }
+
+  track(eventKey: string): void {
+    this.client.isEnabled(eventKey);
+  }
+
+  close(): Promise<void> {
+    this.client.stop();
+
+    return Promise.resolve();
+  }
+}
